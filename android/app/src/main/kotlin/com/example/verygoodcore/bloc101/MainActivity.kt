b6dd0979e7d8f67c27679c00f425c274ab146a13
Paulo13 <107038071+Tomador13@users.noreply.github.com>
2024-08-18 11:03:25 +0800

    chore: first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/bloc101/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.bloc101
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:bloc101/app/app.dart';
+import 'package:bloc101/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
